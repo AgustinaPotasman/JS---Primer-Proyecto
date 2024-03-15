5df72c58e622c42bc61a9741f140f7976dbce379
Gabriel Guivisdalsky <gguivisdalsky@gmail.com>
2024-03-15 11:30:17 -0300

    Continuacion

--- script.js
+++ script.js
@@ -1,51 +1,72 @@
 
-const CalcPromedio = () => {
-    let materias = document.getElementById("Notas").getElementsByTagName("input").length; 
-    let notas = incializar();
-    if(notas.length > 0){
-        let sumNotas = 0;
-        notas.forEach(nota => Number(nota) += sumNotas);
-        let promedio = sumNotas / materias;
-        let resultado =  document.getElementById("resultado");
-        resultado.innerText = promedio;
-    }
-}
 const btnPromedio = document.getElementById("promedio");
 const btnNota = document.getElementById("NotaAlta");
 const botones = document.getElementsByClassName("btn");
+const matematica = document.getElementById("NotaMate");
+const lengua = document.getElementById("NotaLengua");
+const efsi = document.getElementById("NotaEfsi");
 
-const incializar = () => {
-    matematica = document.getElementById("NotaMate").value;
-    lengua = document.getElementById("NotaLengua").value;
-    efsi = document.getElementById("NotaEfsi").value;
-    let notasXMateria = [matematica,lengua,efsi];
-    let estado = true;
-    let popUp = false;
-    notasXMateria.forEach(nota => {
-        console.log(nota);
-        console.log(typeof nota);
-        if((nota.replace(/\s/g, '').length == 0 || parseInt(nota.replace(/\s/g, ''), 10) > 10) && popUp ==  false){
-            if(nota.replace(/\s/g, '').length == 0) alert("Todos los campos deben ser respondidos.");
-            if(parseInt(nota.replace(/\s/g, ''), 10) > 10) alert("No existen notas mayores a 10!");
-            estado = false;
-            popUp = true;
+
+const checkInput = (inputVal) => {
+    if(parseInt((inputVal.value), 10) <= 0 || parseInt((inputVal.value), 10) >= 11 || isNaN(inputVal.value) == true){
+        inputVal.style.color = "red";
+    }else{
+        inputVal.style.color = "green";
+    }
+}
+
+
+matematica.onkeyup = () => {
+    checkInput(matematica);
+}
+
+lengua.onkeyup = () => {
+    checkInput(lengua);
+}
+
+efsi.onkeyup = () => {
+    checkInput(efsi);
+}
+
+const checkIniciar = () => {
+    let materias = [matematica, lengua, efsi];
+    let emptyString = false;
+    let notAllowedString = false;
+    materias.forEach(materia => {
+        if(materia.value.length == 0){
+            emptyString = true;
         }
-    });
-    if(estado == true) return notasXMateria;
+        if(materia.style.color == "red"){
+            notAllowedString = true;
+        }
+    })
+    if(emptyString == true || notAllowedString == true){
+        alert("Todos los campos deben estar rellenos correctamente.");
+        return false;
+    }else{
+        return true;
+    }
+
 }
 
 btnPromedio.onclick = () => {
-    CalcPromedio();
+    if(checkIniciar()){
+        CalcPromedio();
+    }else{
+        console.log(checkIniciar());
+    }
 }
 
 btnNota.onclick = () => {
-    NotaMayor();
+    if(checkIniciar()){
+        NotaMayor();
+    }
 }
 
 const NotaMayor = () => {
     let index = [];
     let materias = ["Matematica", "Lengua", "EFSI"]
-    let notas = incializar();
+    let notas = [matematica.value, lengua.value, efsi.value];
     if(notas.length > 0){
         let i=notas.length - 1;
         let target = Math.max(...notas);
@@ -56,10 +77,35 @@ const NotaMayor = () => {
             i--;
         }
         let resultado =  document.getElementById("resultado");
-        resultado.innerText = "Materia con mayor nota:"
-        index.forEach(element => {
-            resultado.innerText += (" " + materias[element]);
-        });
+        resultado.style.color = "blue";
+        if(index.length > 1){
+            resultado.innerText = "Materias con mayor nota: ";
+        }else{
+            resultado.innerText = "Materia con mayor nota: ";
+        }
+
+        for (let i = 0; i < index.length; i++) {
+            resultado.innerText += (" " + materias[i]);
+            if(i < index.length -1){
+                resultado.innerText += ", ";
+            }                
+        }
     }
 
+}
+
+
+const CalcPromedio = () => {
+    let materias = document.getElementById("Notas").getElementsByTagName("input").length; 
+    let notas = [matematica.value, lengua.value, efsi.value];
+    let sumNotas = 0;
+    notas.forEach(nota => sumNotas += parseInt(nota,10));
+    let promedio = sumNotas / materias;
+    let resultado =  document.getElementById("resultado");
+    if(promedio >= 6){
+        resultado.style.color = "green";
+    }else{
+        resultado.style.color = "red";
+    }
+    resultado.innerText = "Tu promedio es de: " + promedio;
 }
